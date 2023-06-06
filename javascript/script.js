4823fedbde398fa10b94ef5b49c22c78f535e565
anuragkumart16 <anuragkumartiwari12@gmail.com>
2023-06-06 09:59:01 +0530

    completed mobile navbar

--- javascript/script.js
+++ javascript/script.js
@@ -14,20 +14,33 @@ var a=0;
  // eventlisteners
  img.addEventListener(click,toggle);
  
- 
+// subfuctions
+function toggle_after (){
+    nav.style.display="block";
+    navdivleft.style.display="none";
+    navdivright.classList.add("navmobile");
+    home.style.display="block";
+    projects.style.display="block";
+    contact.style.display="block";
+    nav.style.transition="all 2s";
+}
+function toggle_before(){
+    nav.style.display="flex";
+    navdivleft.style.display="block";
+    navdivright.classList.remove("navmobile");
+    home.style.display="none";
+    projects.style.display="none";
+    contact.style.display="none";
+}
  
  // functions
  function toggle(){
     if (a==0){
-        nav.style.display="block";
-        navdivleft.style.display="none";
-        navdivright.classList.add("navmobile");
+        toggle_after()
         a=1;
     }
     else if(a==1) {
-        nav.style.display="flex";
-        navdivleft.style.display="block";
-        navdivright.classList.remove("navmobile");
+        toggle_before()
         a=0;
     }
  }
