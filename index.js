46791f5b36dc624f8e19da08732d4ccafb94093d
anuragkumart16 <anuragkumartiwari12@gmail.com>
2024-05-13 20:28:49 +0530

    added new design

---
+++ index.js
@@ -0,0 +1,19 @@
+function emailme(){
+    window.location.href= "mailto:anuragkumartiwari12@gmail.com";
+}
+function project(){
+   window.location.href='projects.html';
+}
+
+function ig(){
+    window.location.href='https://www.instagram.com/anuragkumartiwari16/';
+ }
+ function fb(){
+    window.location.href='https://www.facebook.com/bpul.tiwari';
+ }
+ function git(){
+    window.location.href='https://github.com/anuragkumart16';
+ }
+ function linkedin(){
+    window.location.href='https://www.linkedin.com/in/anurag-kumar-tiwari-915096262/';
+ }
